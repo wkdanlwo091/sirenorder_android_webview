5d93a9d5b1d683c94e40a94ae18992a5a2bb6033
minjae_Jang <wkdanlwo091@gmail.com>
2020-07-10 21:54:13 +0900

    onMessageReceived update

---
+++ app/src/main/java/com/example/sirenorder/MyFirebaseMessagingService.java
@@ -0,0 +1,66 @@
+package com.example.sirenorder;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private String TAG = "---";
+
+    @Override
+    public void onNewToken(String refreshedToken) {// onNewToken이란 새 토큰이 생성될 때마다 onNewToken 콜백이 호출됩니다.
+
+        super.onNewToken(refreshedToken);
+        Log.e(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        Log.e(TAG, "here ! sendRegistrationToServer! token is " + token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {//메시지 받으면
+        if (remoteMessage.getNotification() != null) {
+            Log.d("FCM Log", "알림 메시지 : " + remoteMessage.getNotification().getBody());
+            String messageBody = remoteMessage.getNotification().getBody();
+            String messageTitle = remoteMessage.getNotification().getTitle();
+
+            Intent intent = new Intent(this, MainActivity.class);// intent란 컴포넌트간에 통신을 하기위한 객체
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);//
+            String channelId = "Channel ID";
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(messageTitle)
+                    .setContentTitle(messageBody)
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                String channelName = "Channel Name";
+                NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+                notificationManager.createNotificationChannel(channel);
+            }
+            notificationManager.notify(0,notificationBuilder.build());
+        }
+    }
+}
