3cb5af71c590467049aaff1437a14584ad947841
minjae_Jang <wkdanlwo091@gmail.com>
2020-07-13 21:59:19 +0900

    webview fixed

--- app/src/main/java/com/example/sirenorder/MainActivity.java
+++ app/src/main/java/com/example/sirenorder/MainActivity.java
@@ -56,9 +56,55 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG,"setWebViewClient changed ");
                 }
             }
-        ); // 클릭시 새창 안뜨게
+        );
 
          */
+
+        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView.addJavascriptInterface(new Object()
+        {
+            @JavascriptInterface           // For API 17+
+            public void performClick(String strl)
+            {
+
+                Log.d("idpassword", strl);
+                //여기서 토큰이랑 id , 비번을 보낸다.
+
+
+
+            }
+        }, "ok");
+
+        mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
+        mWebSettings = mWebView.getSettings(); //세부 세팅 등록
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
+        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+
+        mWebView.loadUrl("http://192.168.43.161"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+
+
+        /*
+        mWebView.addJavascriptInterface(new Object()
+        {
+            @JavascriptInterface           // For API 17+
+            public void performClick(String strl)
+            {
+
+                Log.d("idpassword", strl);
+                //여기서 토큰이랑 id , 비번을 보낸다.
+
+
+
+            }
+        }, "ok");
 
         mWebSettings = mWebView.getSettings(); //html에서 자바스크립트와 상호작용
         mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
@@ -71,19 +117,9 @@ public class MainActivity extends AppCompatActivity {
         mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
         mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
         mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
-        mWebView.addJavascriptInterface(new Object()
-        {
-            @JavascriptInterface           // For API 17+
-            public void performClick(String strl)
-            {
 
-                Log.d("idpassword", strl);
-
-            }
-        }, "ok");
-
-        mWebView.loadUrl("http://192.168.43.161"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
-
+        mWebView.loadUrl("http://192.168.43.161:80"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작// 나중에 aws 주소
+        */
 
     }
 }
